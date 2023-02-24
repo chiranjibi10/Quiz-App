bd783c5961912208f789827cb0a9da0e9d0dc0df
chiranjibi <ranachiranjibi112233@gmail.com>
2023-02-24 12:29:30 +0545

    question added

--- app/src/main/java/com/example/kbcquiz/MainActivity.java
+++ app/src/main/java/com/example/kbcquiz/MainActivity.java
@@ -1,14 +1,62 @@
-package com.example.kbcquiz;
+ package com.example.kbcquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView totalQuestionsTextview;
+    TextView questionsTextview;
+    Button ansA,ansB,ansC,ansD;
+    Button submitBtn;
+
+    int score=0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer ="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalQuestionsTextview = findViewById(R.id.total_question);
+        questionsTextview = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+
+
+
+        totalQuestionsTextview.setText("Total Question:"+totalQuestion);
+
+        loadNewQuestion();
+
+
     }
-}
+
+     @Override
+     public void onClick(View view) {
+
+     }
+     void loadNewQuestion(){
+        questionsTextview.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+         ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+         ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+         ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+     }
+ }
---
+++ app/src/main/java/com/example/kbcquiz/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.kbcquiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Who is the father of Economics ?",
+            "What is the capital city of Nepal ?",
+            "Which color symbolizes peace ?",
+            "Who is the first prime minister of Nepal ?",
+
+
+    };
+    public static String choices[][] ={
+            {"Adam Smith","George Washington","Newton","Adam Gilchrist"},
+            {"Pokhara","Kathmandu","Biratnagar","Dharan"},
+            {"Black","Red","purple","White"},
+            {"Prachanda","Bhimsen Thapa","Ram baran yadav","Baburam Bhattarai"}
+
+    };
+
+    public static  String correctAnswer[] ={
+            "Adam Smith",
+            "Kathmandu",
+            "White",
+            "Bhimsen Thapa"
+
+    };
+}
